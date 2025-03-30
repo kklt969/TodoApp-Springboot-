d7dcea4821a272e5d99b0c36a7861ef46651c4ed
kklt969 <167519863+kklt969@users.noreply.github.com>
2025-03-29 20:29:00 -0700

    Initial commit

---
+++ src/main/java/com/webapp/webapp_kklt/WebappKkltApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webapp_kklt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappKkltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappKkltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/webapp_kklt/WebappKkltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.webapp_kklt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappKkltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
